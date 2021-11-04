4d7b4ecce0055dd768a92e0a27182f0c3bc14895
Jeff Paulin <jmpaulin13@gmail.com>
2021-11-04 12:54:18 -0400

    finished the basic tutorial. Will not be doing the desktop app

---
+++ UI/ButtonAndEntry.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+entry = ttk.Entry(root, width=50)
+entry.pack()
+
+button = ttk.Button(root, text="Click me")
+button.pack()
+
+
+
+
+
+root.mainloop()
